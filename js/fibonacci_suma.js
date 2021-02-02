cca98a8d9765c56bfa61803bb66d84d8d1165d90
Karl Heitmann <khf1988@gmail.com>
2021-02-02 19:01:55 -0300

    listo con fibonacci_suma.html

---
+++ js/fibonacci_suma.js
@@ -0,0 +1,30 @@
+function fibonacci_suma(n) {
+  let sum = 0;
+  let n_2 = 0;
+  let n_1 = 1;
+  if (n == 1) {
+    return 1;
+  } else if (n == 2) {
+    return 1;
+  } else if (n > 2) {
+    for (let i = 1; i < n; i++) {
+      sum = n_2 + n_1;
+      n_2 = n_1;
+      n_1 = sum
+    }
+    return sum;
+  } else {
+    return 0;
+  }
+}
+
+let f_n = 7;
+
+console.log(`Fibonacci ${f_n}: ${fibonacci_suma(f_n)}`);
+
+try {
+  document.write(`Fibonacci ${f_n}: ${fibonacci_suma(f_n)}`);
+} catch (error) {
+  console.log("NO BROWSER");
+}
+

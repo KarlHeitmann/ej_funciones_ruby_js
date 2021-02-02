819db4dca3697c7a59deb0402232204f477451cb
Karl Heitmann <khf1988@gmail.com>
2021-02-02 18:53:31 -0300

    fibonacci html terminado

---
+++ js/fibonacci.js
@@ -0,0 +1,21 @@
+function fibo(n) {
+  if (n == 1) {
+    return 1;
+  } else if (n == 2) {
+    return 1;
+  } else if (n > 2) {
+    return fibo(n-1) + fibo(n-2);
+  } else {
+    return 0;
+  }
+}
+
+let f_n = 6
+
+console.log(`Fibonacci ${f_n}: ${fibo(f_n)}`);
+
+try {
+  document.write(`Fibonacci ${f_n}: ${fibo(f_n)}`);
+} catch (error) {
+  console.log("NO BROWSER");
+}
